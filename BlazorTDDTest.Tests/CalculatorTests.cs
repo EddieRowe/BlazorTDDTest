7e1f8622b300d9ec523b58d1563a1d8e80487e31
Eddie Rowe <edrowe@pm.me>
2023-11-21 08:19:44 +0000

    Create calculator

---
+++ BlazorTDDTest.Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using BlazorTDDTest.Shared;
+
+namespace BlazorTDDTest.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void ShouldDisplayZeroOnLoad()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Calculator>();
+            var paraElm = cut.Find("p");
+
+            // Assert
+            var paraElmText = paraElm.TextContent;
+            paraElmText.MarkupMatches("0");
+        }
+    }
+}
