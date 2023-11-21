0afc4798f04565000f61f2b6f5504089107bb77c
Eddie Rowe <edrowe@pm.me>
2023-11-21 08:43:08 +0000

    Add Tests

--- BlazorTDDTest.Tests/CalculatorTests.cs
+++ BlazorTDDTest.Tests/CalculatorTests.cs
@@ -5,7 +5,7 @@ namespace BlazorTDDTest.Tests
     public class CalculatorTests
     {
         [Fact]
-        public void ShouldDisplayZeroOnLoad()
+        public void DisplayShouldShowZeroOnLoad()
         {
             // Arrange
             using var ctx = new TestContext();
@@ -14,7 +14,56 @@ namespace BlazorTDDTest.Tests
 
             // Assert
             var paraElmText = paraElm.TextContent;
-            paraElmText.MarkupMatches("0");
+            paraElmText.MarkupMatches(": 0");
+        }
+
+        [Fact]
+        public void DisplayShouldBindToCode() {
+            // Arrange
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Calculator>();
+            var paraElm = cut.Find("p");
+
+            //Act
+            cut.Find("button#one").Click();
+
+            // Assert
+            var paraElmText = paraElm.TextContent;
+            paraElmText.MarkupMatches(": 1");
+        }
+
+        [Fact]
+        public void DisplayShouldAddToStringIfNonZero()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Calculator>();
+            var paraElm = cut.Find("p");
+
+            //Act
+            cut.Find("button#one").Click();
+            cut.Find("button#one").Click();
+
+            // Assert
+            var paraElmText = paraElm.TextContent;
+            paraElmText.MarkupMatches(": 11");
+        }
+
+        [Fact]
+        public void ClearButtonShouldClearDisplay()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Calculator>();
+            var paraElm = cut.Find("p");
+
+            // Act
+            cut.Find("button#one").Click();
+            cut.Find("button#clear-display").Click();
+
+            // Assert
+            var paraElmText = paraElm.TextContent;
+            paraElmText.MarkupMatches(": 0");
         }
     }
 }
